72f5300577c5a0bd627e50be8798ef0b72d2ec67
daily1313 <kimsb7218@naver.com>
2025-02-15 22:45:25 +0900

    first commit

---
+++ src/test/java/com/example/tabletennis/TabletennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabletennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletennisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
