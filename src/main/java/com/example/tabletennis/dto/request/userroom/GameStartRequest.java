9b6a976ed052f480ed2723c0e61f8b0f9edd978b
daily1313 <kimsb7218@naver.com>
2025-02-19 00:21:52 +0900

    refactor: remove roomId parameter in dto

--- src/main/java/com/example/tabletennis/dto/request/userroom/GameStartRequest.java
+++ src/main/java/com/example/tabletennis/dto/request/userroom/GameStartRequest.java
@@ -2,6 +2,5 @@ package com.example.tabletennis.dto.request.userroom;
 
 import jakarta.validation.constraints.NotNull;
 
-public record GameStartRequest(@NotNull Integer userId,
-                               @NotNull Integer roomId) {
+public record GameStartRequest(@NotNull Integer userId) {
 }
